6b4599c8763fd70f2cc6179d7eaac76ddab66b66
kinoppol <kinoppol@hotmail.com>
2022-11-24 10:05:17 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,6 +5,9 @@ require_once('main/config/setup.php');
 
 $db = new mysqli($db_host,$db_user,$db_pass,$db_database);
 
+if(!$db){
+    print "Can't connect db"
+}
 
 function pq($str,$force=false){
     if((is_numeric($str)||empty($str))&&!$force) return $str;
