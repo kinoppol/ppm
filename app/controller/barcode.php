369af7f0aee1a149298142116004142da799e4ee
kinoppol <kinoppol@hotmail.com>
2022-12-07 20:37:53 +0700

    Create barcode.php

---
+++ app/controller/barcode.php
@@ -0,0 +1,17 @@
+<?php
+class barcode{
+    function index(){
+    }
+    function search(){
+        $store=model('store');
+        $stores=$store->get_store(array('id'=>$_SESSION['user']['store_id'],'status'=>'operated'));
+        $data['title']=$stores[0]['name'];
+        $data['store_name']=$stores[0]['name'];
+        $data['sub_name']=$stores[0]['sub_name'];
+
+        $data['content']=$_GET['q'];
+        return view('template/main',$data);
+    }
+    function daily_report(){
+    }
+}
