30eceb1e3a82ef6484a0e12d730466324a390cd0
kinoppol <kinoppol@hotmail.com>
2022-12-07 20:41:26 +0700

    Update barcode.php

--- app/controller/barcode.php
+++ app/controller/barcode.php
@@ -3,13 +3,7 @@ class barcode{
     function index(){
     }
     function search(){
-        $store=model('store');
-        $stores=$store->get_store(array('id'=>$_SESSION['user']['store_id'],'status'=>'operated'));
-        $data['title']=$stores[0]['name'];
-        $data['store_name']=$stores[0]['name'];
-        $data['sub_name']=$stores[0]['sub_name'];
-
         $data['content']=$_GET['q'];
-        return view('template/main',$data);
+        return view('template/authen',$data);
     }
 }
