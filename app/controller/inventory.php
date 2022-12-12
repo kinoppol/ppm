6a31f46f00269437ebebdd0026147af06a49644c
kinoppol <kinoppol@hotmail.com>
2022-12-12 13:26:11 +0700

    Update inventory.php

--- app/controller/inventory.php
+++ app/controller/inventory.php
@@ -32,7 +32,7 @@ class inventory{
         }
         $data=array();
         $data['content']=view('product/form_product',$result);
-        if($result['picture']!='NULL'){
+        if($result['picture']!=''){
             $data['content'].=view('product/gallery',array('pictures'=>json_decode($result['picture'],true)));
         }
         return view('template/main',$data);
