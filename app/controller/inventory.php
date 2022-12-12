ea342521a1fb9e0abeab0eec6d6b1b9acd96a31f
kinoppol <kinoppol@hotmail.com>
2022-12-12 13:40:26 +0700

    Update inventory.php

--- app/controller/inventory.php
+++ app/controller/inventory.php
@@ -97,7 +97,7 @@ class inventory{
             'expiry_date'=>$_POST['expiry_date'],
             'date_arrival'=>$_POST['date_arrival'],
         );
-        if(!empty($_FILES['picture'])){
+        if(!empty($_FILES['picture'])&&count($_FILES['picture']['name'])>0){
             helper('upload');
             helper('image');
             $store_id=$_SESSION['user']['store_id'];
