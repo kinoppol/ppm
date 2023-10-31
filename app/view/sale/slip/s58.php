08b3c8f1d2655ae867d65a2204e5abe51fecaeb4
kinoppol <kinoppol@hotmail.com>
2023-10-31 07:07:06 +0700

    Update s58.php

--- app/view/sale/slip/s58.php
+++ app/view/sale/slip/s58.php
@@ -3,13 +3,19 @@
 	<div style="width: 100%; float: left;">
 <div style="text-align:center">
 <h2>Wind Mill</h2>
+สาขาวิทยาลัยเกษตรและเทคโนโลยีร้อยเอ็ด</br>
 เลขที่ 235 หมู่ 4 ต. นิเวศน์<br> 
 อ. ธวัชบุรี จ. ร้อยเอ็ด 
 </div><br> 
-<?php print $inv; ?><br><br> 
+<?php print $inv; ?><br>
 - - - - - - - - - - - - - - - - - - - - - -
-<br><br>
-10001 ICE-TEA 2@15 = 30
+<br>
+1 ICE-TEA 2@15 = 30<br>
+- - - - - - - - - - - - - - - - - - - - - -
+<br>
+รวม 30 บาท<br>
+ประเภทการชำระ QR
+<br>
 </div>
 </div>
 </div>
